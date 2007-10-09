a35967104166b0b33d08659385f81472cfe5ca0d
michaelvantellingen <michaelvantellingen@32760e9c-fc3b-0410-b7a5-4bea93d8f68f>
2007-10-09 12:16:52 +0000

    create a dict which contains a structure which should be used to decide which action should be taken on which atom.

--- trunk/videoparser/plugins/mov.py
+++ trunk/videoparser/plugins/mov.py
@@ -29,35 +29,62 @@
 
 """
 
-
-
-# Only implement required information to retrieve video and audio information
-
-atom_container = 1
-atom_leaf      = 2
-
-#atom_types = {
-#    
-#    'moov':     ('Movie atom',{
-#        'trak':     ('Track atom', {
-#            'tkhd':     ('Track header atom'),
-#        
-#            'mdia':     ('Media atom', {
-#                    'mdhd':     ('Media Header atom'),
-#                    'hdlr':     ('Media handler reference atom')
-#                    'minf':     ('Video media information atom')
-#                        
-#                    },
-#                         
-#                 
-#    
-#    'edts':
-#    'vmhd':
-#    'minf':
-#    'keys':
-#    'udta':
-#    'ilst':
-#    'tmcd':
+# Define the structure of the movie atom
+atom_structure = {
+    'moov':     ('Movie atom', {
+        'mvhd':     ('Movie header atom'),
+        'trak':     ('Track atom', {
+            'tkhd':      ('Track header atom'),
+            'clip':      ('Track clipping atom', {
+                'crgn':     ('Clipping region atom'),
+            }),
+            'matt':      ('Track matte atom', {
+                'kmat':     ('Compressed matte atomm'),
+            }),
+            'edts':      ('Edit atom', {
+                'elst':     ('Edit list atom'),
+            }),
+            'tref':      ('DescriptionHere', {
+                'tmcd':       ('DescriptionHere')
+            }),
+            'mdia':      ('Media atom', {
+                'mdhd':       ('Media header atom'),
+                'hdlr':       ('Media handler reference atom'),
+                'minf':       ('Video media information atom', {
+                    'smhd':    ('DescriptionHere'),
+                    'gmhd':    ('DescriptionHere'),
+                    'vmhd':        ('Video media information header atom'),
+                    'hdlr':        ('Data handler reference atom'),
+                    'dinf':        ('Data information atom', {
+                        'dref':         ('Data reference atom')
+                    }),
+                    'stbl':        ('Sample table atom', {
+                        'stsd':         ('Sample description atom'),
+                        'stts':         ('Time-to-sample atom'),
+                        'stsc':         ('Sample-to-chunk atom'),
+                        'stsz':         ('Sample size atom'),
+                        'stco':         ('Chunk offset atom'),
+                    }),
+                }),
+            }),
+            'udta':    ('DescriptionHere'),
+            'meta':    ('DescriptionHere', {
+                'hdlr':    ('DescriptionHere'),
+                'keys':    ('DescriptionHere'),
+                'ilst':    ('DescriptionHere'),
+            }),
+        }),
+        'udta':    ('User data atom'),
+        'meta':    ('Metadata atom (Guess?)', {
+            'hdlr':    ('Undocumented (HDLR)'),
+            'keys':    ('Undocumented (KEYS)'),
+            'ilst':    ('Undocumented (ILST)'),
+        }),
+        'cmov':     ('Color table atom'),
+        'cmov':     ('Compressed movie atom'),
+        'rmra':     ('Reference movie atom'),
+    })
+}
     
 # For running single
 if __name__ == "__main__":
@@ -69,36 +96,29 @@ import plugins
 import streams
 
 
-
 class Parser(plugins.BaseParser):
     _endianess = streams.BIG_ENDIAN
     
     def __init__(self):
         plugins.BaseParser.__init__(self)
-        
+        self._parse_level = 0
 
     def parse(self, filename, video):
         
         stream = streams.FileStream(filename, endianess=self._endianess)
 
 
-        for i in range(2):
-            try:
-                atom_size = stream.read_uint32()
-            except:
-                return
+        for i in range(4):
+            atom_size = stream.read_uint32()
             atom_type = stream.read(4)
-            atom_data = stream.read_subsegment(atom_size - 8)
             
-            print atom_size, atom_type            
+            type_data = atom_structure.get(atom_type)
 
-            
-            #if atom_type == 'ftyp':
-                #self.parse_ftyp(atom_data)
-            
-            if atom_type == 'moov':
-                self.parse_atom(atom_data)
-        
+            if type(type_data) == tuple:
+                atom_data = stream.read_subsegment(atom_size - 8)
+                self.parse_atom(atom_data, atom_tree=type_data[1])
+            else:
+                stream.seek(stream.tell() + atom_size - 8)
 
         return True
     
@@ -107,7 +127,8 @@ class Parser(plugins.BaseParser):
         print repr(data)
         
     
-    def parse_atom(self, data, level=0):
+    def parse_atom(self, data, atom_tree=None):
+        self._parse_level += 1
         while data.bytes_left():
             
             atom_data = None
@@ -115,21 +136,24 @@ class Parser(plugins.BaseParser):
             atom_type = data.read(4)
             atom_data = data.read_subsegment(atom_size - 8)
 
-            print "   " * (level + 1), atom_size, atom_type
+            #print "   " * (level + 1), "'%s':    ('DescriptionHere', {" % atom_type
             
-            if atom_type in ['mvhd', 'tkhd', 'mdhd', 'hdlr', 'edts', 'vmhd', 
-                             'dref', 'stts', 'stsd', 'stco','stsc','stsz', 'stss',
-                             'keys', 'udta', 'ilst', 'tmcd', 'gmhd', 'gmin',
-                             'smhd', 'dinf',
-                             'taft', 'clef', 'prof', 'enof' #TrackApertureModeDimensionsAID
-                             ]:
-                
-                pass # Process data items
+            atom_tree_item = atom_tree.get(atom_type)
             
+            if not atom_tree_item:
+                print "  " * (self._parse_level +1 ), "Uknown item!!!!"
+                continue
             
+            if type(atom_tree_item) == tuple:
+                print "  " * (self._parse_level +1 ), atom_tree_item[0]
             else:
-                self.parse_atom(atom_data, level  +1)
+                print "  " * (self._parse_level +1 ), atom_tree_item
             
+            if type(atom_tree_item) == tuple:
+                self.parse_atom(atom_data, atom_tree=atom_tree_item[1])
+            else:
+                pass
+        self._parse_level -= 1
         
     def extract_information(self, header, video):
         
