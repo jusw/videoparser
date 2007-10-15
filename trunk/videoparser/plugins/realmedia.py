e7ef3e4e5553dd40c9f6f4dab3bc01008f9438da
michaelvantellingen <michaelvantellingen@32760e9c-fc3b-0410-b7a5-4bea93d8f68f>
2007-10-15 11:40:54 +0000

    update parser, add parsing support for the specific_type_data when mimetype = video/x-pn-realvideo. This is still incomplete and completly based on guessing, so there might be some bugs

--- trunk/videoparser/plugins/realmedia.py
+++ trunk/videoparser/plugins/realmedia.py
@@ -33,7 +33,7 @@
 import struct
 import cStringIO
 
-
+import time
 # For testing
 if __name__ == "__main__":
     import sys; sys.path.append('../../'); sys.path.append('..')
@@ -42,6 +42,11 @@ if __name__ == "__main__":
 import videoparser.plugins as plugins
 import videoparser.streams as streams
 
+
+
+mime_types = {
+    'audio/x-pn-realaudio':     'audio',
+}
 
 class Parser(plugins.BaseParser):
     _endianess = streams.endian.big
@@ -53,70 +58,148 @@ class Parser(plugins.BaseParser):
     def parse(self, filename, video):
         stream = streams.factory.create_filestream(filename,
                                                    endianess=self._endianess)
+        
+        data = self.parse_objects(stream)
 
-        self.parse_objects(stream)
 
+        # Extract required information from the tree and place it in the
+        # videofile object
+        self.extract_information(data, video)
+        
+    
+    def extract_information(self, data, video):
+        
+        for object in data:
+            
+            # Extract stream info
+            if isinstance(object, self.MediaProperties):
+                if object.mime_type == 'logical-fileinfo':
+                    continue
+                if object.mime_type != 'video/x-pn-realvideo':
+                    continue
+                
+                
+
+                    
         
     
     def parse_objects(self, stream):
-
+        objects = []
+        
         while stream.bytes_left():
-            id = stream.read_dword()
+            id = stream.read_fourcc()
             size = stream.read_uint32()
-            data = stream.read_subsegment(size - 8)
+            
+            # It seems some files have random junk data after the INDX object
+            if id == '\x00\x00\x00\x00':
+                break
+
+            if id in ['.RMF', 'PROP', 'MDPR', 'CONT']: #, 'DATA']:
+                data = stream.read_subsegment(size - 8)
+            else:
+                stream.seek(stream.tell() + size - 8)
+                continue
             
             if id == '.RMF':
-                obj = self.parse_header(data)
+                obj = self._parse_realmedia_file(data)
             
             elif id == 'PROP':
-                obj = self.parse_fileproperties(data)
+                obj = self._parse_fileproperties(data)
             
             elif id == 'MDPR':
-                obj = self.parse_mediaproperties(data)
+                obj = self._parse_mediaproperties(data)
             
             elif id == 'CONT':
-                obj = self.parse_contentdescription(data)
+                obj = self._parse_contentdescription(data)
             
-            elif id == 'DATA':
-                obj = None
+            #elif id == 'DATA':
+            #    for i in range(0, 2):
+            #        print repr(data.read(10))
+            objects.append(obj)
             
-            elif id == 'INDX':
-                obj = None
-        
-            print ">> ", obj
-        return True
+        return objects
 
 
-    def parse_header(self, data):
+    def _parse_realmedia_file(self, data):
+        """  RealMedia file header (.RMF) """
         return None
 
-    def parse_fileproperties(self, data):
+    def _parse_fileproperties(self, data):
+        """ Parse the File properties header (PROP) """
         obj = self.FileProperties()
 
         obj.version = data.read_uint16()
         
         if obj.version == 0:
-          obj.max_bit_rate = data.read_uint32()
-          obj.avg_bit_rate = data.read_uint32()
-          obj.max_packet_size = data.read_uint32()
-          obj.avg_packet_size = data.read_uint32()
-          obj.num_packets = data.read_uint32()
-          obj.duration = data.read_uint32()
-          obj.preroll = data.read_uint32()
-          obj.index_offset = data.read_uint32()
-          obj.data_offset = data.read_uint32()
-          obj.num_streams = data.read_uint16()
-          obj.flags = data.read_uint16()
+            obj.max_bit_rate = data.read_uint32()
+            obj.avg_bit_rate = data.read_uint32()
+            obj.max_packet_size = data.read_uint32()
+            obj.avg_packet_size = data.read_uint32()
+            obj.num_packets = data.read_uint32()
+            obj.duration = data.read_uint32()
+            obj.preroll = data.read_uint32()
+            obj.index_offset = data.read_uint32()
+            obj.data_offset = data.read_uint32()
+            obj.num_streams = data.read_uint16()
+            obj.flags = data.read_uint16()
     
         return obj
     
-    def parse_mediaproperties(self, data):
+    def _parse_mediaproperties(self, data):
+        """ Parse the  Media properties header (MDPR)"""
+        obj = self.MediaProperties()
+        obj.version = data.read_uint16()
+
+        if obj.version == 0:
+            obj.stream_number = data.read_uint16()
+            obj.max_bit_rate = data.read_uint32()
+            obj.avg_bit_rate = data.read_uint32()
+            obj.max_packet_size = data.read_uint32()
+            obj.avg_packet_size = data.read_uint32()
+            obj.start_time = data.read_uint32()
+            obj.preroll = data.read_uint32()
+            obj.duration = data.read_uint32()
+            obj.stream_name_size = data.read_uint8()
+            obj.stream_name = data.read(obj.stream_name_size)
+            obj.mime_type_size = data.read_uint8()
+            obj.mime_type = data.read(obj.mime_type_size)
+            obj.type_specific_len = data.read_uint32()
+            
+            type_data = data.read_subsegment(obj.type_specific_len)
+            
+            if obj.mime_type == 'video/x-pn-realvideo':
+                obj.type_specific_data = self._parse_type_realvideo(type_data)
+            else:
+                obj.type_specific_data = type_data
+            
+        return obj
+
+
+    def _parse_type_realvideo(self, s):
+        
+        # This is based on guessing, so it might not be 100% ok.
+        # i still need to find the framerate (atleast i suppose it is included)
+        
+        print "version?", s.read_uint16()
+        
+        print "size %r" % s.read_uint16()
+        print "type", s.read_fourcc()
+        print "fourcc", s.read_fourcc()
+        print "width", s.read_uint16()
+        print "height", s.read_uint16()
+        print "12 = ", s.read_uint16()
+        print "0 = ", s.read_uint16()
+        print "0 = ", s.read_uint16()
+        print "fps = ", s.read_qtfloat_32()
+        
+
+                
+    def _parse_contentdescription(self, data):
+        """ Parse the  Content description header (CONT)"""
         return None
-    
-    def parse_contentdescription(self, data):
-        return None
-    
-    
+
+
+
     
     class FileProperties(object):
         def __repr__(self):
@@ -137,6 +220,33 @@ class Parser(plugins.BaseParser):
             return buffer
         
         
+    class MediaProperties(object):
+        def __repr__(self):
+            buffer  = "MediaProperties structure:\n"
+            buffer += " %-30s: %s\n" % ('Version', self.version)
+            buffer += " %-30s: %s\n" % ('Stream number', self.stream_number)
+            buffer += " %-30s: %s\n" % ('Max Bitrate', self.max_bit_rate)
+            buffer += " %-30s: %s\n" % ('Avg Bitrate', self.avg_bit_rate)
+            buffer += " %-30s: %s\n" % ('Max packet size',
+                                        self.max_packet_size)
+            buffer += " %-30s: %s\n" % ('Avg packet size',
+                                        self.avg_packet_size)
+            buffer += " %-30s: %s\n" % ('Start time', self.start_time)
+            buffer += " %-30s: %s\n" % ('Preroll', self.preroll)
+            buffer += " %-30s: %s\n" % ('Duration', self.duration)
+            buffer += " %-30s: %s\n" % ('Stream name length',
+                                        self.stream_name_size)
+            buffer += " %-30s: %s\n" % ('Stream name', self.stream_name)
+            buffer += " %-30s: %s\n" % ('Mime type length',
+                                        self.mime_type_size)
+            buffer += " %-30s: %s\n" % ('Mime type', self.mime_type)
+            buffer += " %-30s: %s\n" % ('Type specific data length',
+                                        self.type_specific_len)
+            buffer += " %-30s: %r\n" % ('Type specific data',
+                                        self.type_specific_data)
+        
+            return buffer
+            
 if __name__ == "__main__":
     import sys
     import videofile
