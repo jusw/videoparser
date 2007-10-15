5354f58723cb6d5295ae5549dfbcb9717ad93b12
michaelvantellingen <michaelvantellingen@32760e9c-fc3b-0410-b7a5-4bea93d8f68f>
2007-10-15 12:52:27 +0000

    add parsing of realaudio extra data. only ra4/ra5 is implemented atm

--- trunk/videoparser/plugins/realmedia.py
+++ trunk/videoparser/plugins/realmedia.py
@@ -70,13 +70,15 @@ class Parser(plugins.BaseParser):
     def extract_information(self, data, video):
         
         for object in data:
-            
-            # Extract stream info
-            if isinstance(object, self.MediaProperties):
-                if object.mime_type == 'logical-fileinfo':
-                    continue
-                if object.mime_type != 'video/x-pn-realvideo':
-                    continue
+            pass
+            #print object
+            #
+            ## Extract stream info
+            #if isinstance(object, self.MediaProperties):
+            #    if object.mime_type == 'logical-fileinfo':
+            #        continue
+            #    if object.mime_type != 'video/x-pn-realvideo':
+            #        continue
                 
                 
 
@@ -169,29 +171,91 @@ class Parser(plugins.BaseParser):
             
             if obj.mime_type == 'video/x-pn-realvideo':
                 obj.type_specific_data = self._parse_type_realvideo(type_data)
+            elif obj.mime_type == 'audio/x-pn-realaudio':
+                obj.type_specific_data = self._parse_type_realaudio(type_data)
             else:
                 obj.type_specific_data = type_data
             
+            print obj.type_specific_data
         return obj
 
 
-    def _parse_type_realvideo(self, s):
+    def _parse_type_realvideo(self, data):
         
         # This is based on guessing, so it might not be 100% ok.
         # i still need to find the framerate (atleast i suppose it is included)
         
-        print "version?", s.read_uint16()
+        obj = self.RealVideoProperties()
+        obj.version = data.read_uint16()
+        obj.size = data.read_uint16()
+        obj.type = data.read_fourcc()
+        obj.codec = data.read_fourcc()
+        obj.width = data.read_uint16()
+        obj.height = data.read_uint16()
         
-        print "size %r" % s.read_uint16()
-        print "type", s.read_fourcc()
-        print "fourcc", s.read_fourcc()
-        print "width", s.read_uint16()
-        print "height", s.read_uint16()
-        print "12 = ", s.read_uint16()
-        print "0 = ", s.read_uint16()
-        print "0 = ", s.read_uint16()
-        print "fps = ", s.read_qtfloat_32()
+        data.seek(data.tell() + 6)
         
+        # Double check if this is correct for RV10/RV20 since all test files
+        # return 15.0
+        obj.fps = data.read_qtfloat_32()
+        
+        # I have no clue what the other bytes mean
+        
+        return obj
+        
+    def _parse_type_realaudio(self, data):
+
+        # Based on information at:
+        # http://wiki.multimedia.cx/index.php?title=RealMedia
+        
+        obj = self.RealAudioProperties()
+        obj.type = data.read_dword()
+        obj.version = data.read_uint16()
+        
+        if obj.version == 3:
+            pass
+        
+        elif obj.version in [4, 5]:
+            obj.unused = data.read_uint16()
+            obj.signature =  data.read(4)
+            obj.unknown_1 = data.read_uint32()
+            obj.version_2 = data.read_uint16()
+            obj.header_size = data.read_uint32()
+            obj.codec_flavor = data.read_uint16()
+            obj.codec_frame_size = data.read_uint32()
+            obj.unknown_2 = data.read(12)
+            obj.sub_packet = data.read_uint16()
+            obj.frame_size = data.read_uint16()
+            obj.sub_packet_size = data.read_uint16()
+            obj.unknown_3 = data.read_uint16()
+        
+            if obj.version == 5:
+                obj.unknown_4 = data.read(6)
+            
+            obj.sample_rate = data.read_uint16()
+            obj.unknown_5 = data.read_uint16()
+            obj.sample_size = data.read_uint16()
+            obj.num_channels = data.read_uint16()
+            
+            if obj.version == 4:
+                obj.interleaver_id_length = data.read_uint8()
+                obj.interleaver_id = data.read(obj.interleaver_id_length)
+                obj.fourcc_string_length = data.read_uint8()
+                obj.fourcc_string = data.read(obj.fourcc_string_length)
+                
+            elif obj.version == 5:
+                obj.interleaver_id = data.read_dword()
+                obj.fourcc_string = data.read_dword()
+
+            obj.unknown_6 = data.read(3)
+            
+            if obj.version == 5:
+                obj.unknown_7 = data.read(1)
+            
+            obj.codec_extradata_length = data.read_uint32()
+            obj.codec_extradata = data.read(obj.codec_extradata_length)
+
+        return obj
 
                 
     def _parse_contentdescription(self, data):
@@ -199,6 +263,15 @@ class Parser(plugins.BaseParser):
         return None
 
 
+
+    class Structure(object):
+        def repr_childs(self, obj):
+            buffer = ""
+            for entry in obj:
+                buffer += "\n".join(["   %s" % line for line
+                                     in repr(entry).split('\n')])
+                buffer += "\n"
+            return buffer
 
     
     class FileProperties(object):
@@ -220,7 +293,7 @@ class Parser(plugins.BaseParser):
             return buffer
         
         
-    class MediaProperties(object):
+    class MediaProperties(Structure):
         def __repr__(self):
             buffer  = "MediaProperties structure:\n"
             buffer += " %-30s: %s\n" % ('Version', self.version)
@@ -242,20 +315,96 @@ class Parser(plugins.BaseParser):
             buffer += " %-30s: %s\n" % ('Mime type', self.mime_type)
             buffer += " %-30s: %s\n" % ('Type specific data length',
                                         self.type_specific_len)
-            buffer += " %-30s: %r\n" % ('Type specific data',
-                                        self.type_specific_data)
+            buffer += " %-30s:\n %s" % ('Type specific data',
+                                        self.repr_childs([self.type_specific_data]))
         
             return buffer
+    
+    
+    class RealVideoProperties(Structure):
+        def __repr__(self):
+            buffer  = "RealVideoProperties structure:\n"
+            buffer += " %-30s: %s\n" % ('Version (?)',self.version)
+            buffer += " %-30s: %s\n" % ('Size',self.size)
+            buffer += " %-30s: %s\n" % ('Type',self.type)
+            buffer += " %-30s: %s\n" % ('Codec',self.codec)
+            buffer += " %-30s: %s\n" % ('Width',self.width)
+            buffer += " %-30s: %s\n" % ('Height',self.height)
+            buffer += " %-30s: %s\n" % ('Frames per second',self.fps)
+    
+            return buffer
+        
+    class RealAudioProperties(Structure):
+        def __repr__(self):
+            buffer  = "RealAudioProperties structure:\n"
+            buffer += " %-30s: %s\n" % ('Type', self.type)
+            buffer += " %-30s: %s\n" % ('Version', self.version)
             
+            if self.version == 3:
+                pass
+            
+            elif self.version in [4, 5]:
+                buffer += " %-30s: %s\n" % ('Unused', self.unused)
+                buffer += " %-30s: %s\n" % ('Signature', self.signature)
+                buffer += " %-30s: %r\n" % ('Unknown', self.unknown_1)
+                buffer += " %-30s: %s\n" % ('Version 2', self.version_2)
+                buffer += " %-30s: %s\n" % ('Header size', self.header_size)
+                buffer += " %-30s: %s\n" % ('Codec flavor', self.codec_flavor)
+                buffer += " %-30s: %s\n" % ('Codec frame size',
+                                            self.codec_frame_size)
+                buffer += " %-30s: %r\n" % ('Unknown', self.unknown_2)
+                buffer += " %-30s: %s\n" % ('Sub-packet', self.sub_packet)
+                buffer += " %-30s: %s\n" % ('Frame size', self.frame_size)
+                buffer += " %-30s: %s\n" % ('Sub-packet size',
+                                            self.sub_packet_size)
+                buffer += " %-30s: %r\n" % ('Unknown', self.unknown_3)
+            
+                if self.version == 5:
+                    buffer += " %-30s: %r\n" % ('Unknown', self.unknown_4)
+                
+                buffer += " %-30s: %s\n" % ('Sample rate', self.sample_rate)
+                buffer += " %-30s: %r\n" % ('Unknown', self.unknown_5)
+                buffer += " %-30s: %s\n" % ('Sample size', self.sample_size)
+                buffer += " %-30s: %s\n" % ('Channels', self.num_channels)
+                
+                if self.version == 4:
+                    buffer += " %-30s: %s\n" % ('Interleaver id length',
+                                                self.interleaver_id_length)
+                    buffer += " %-30s: %s\n" % ('Interleaver id',
+                                                self.interleaver_id)
+                    buffer += " %-30s: %s\n" % ('FourCC string length',
+                                                self.fourcc_string_length)
+                    buffer += " %-30s: %s\n" % ('FourCC string',
+                                                self.fourcc_string)
+                    
+                elif self.version == 5:
+                    buffer += " %-30s: %s\n" % ('Interleaver id', self.interleaver_id)
+                    buffer += " %-30s: %s\n" % ('FourCC string', self.fourcc_string)
+    
+                buffer += " %-30s: %r\n" % ('Unknown', self.unknown_6)
+                
+                if self.version == 5:
+                    buffer += " %-30s: %r\n" % ('Unknown', self.unknown_7)
+                
+                buffer += " %-30s: %s\n" % ('Codec extra data length',
+                                            self.codec_extradata_length)
+                buffer += " %-30s: %r\n" % ('Codec extra data',
+                                            self.codec_extradata)
+    
+            return buffer
+        
+        
 if __name__ == "__main__":
     import sys
     import videofile
     
     video = videofile.VideoFile()
     p = Parser()
-    p.parse(sys.argv[1], video)
+    for arg in sys.argv[1:]:
+        p.parse(arg, video)
 
     print video
     
+
 
 
