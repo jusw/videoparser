7b4fcb9a3677c43b9c128f2f2353d9ea6387fb06
michaelvantellingen <michaelvantellingen@32760e9c-fc3b-0410-b7a5-4bea93d8f68f>
2007-10-15 18:36:10 +0000

    First build a tree of all values in the matroska file based on the level. After that we can extract the information required more easily. This is a small performance hit, but should result in cleaner code eventually ;p

--- trunk/videoparser/plugins/matroska.py
+++ trunk/videoparser/plugins/matroska.py
@@ -36,6 +36,11 @@ from __future__ import generators
 # Python built-in modules
 import struct
 
+
+# For testing
+if __name__ == "__main__":
+    import sys; sys.path.append('../../'); sys.path.append('..')
+
 # Project modules
 import videoparser.plugins as plugins
 import videoparser.streams as streams
@@ -52,62 +57,63 @@ types = Types()
 
 # Only implement required information to retrieve video and audio information
 
-#   Class-id        Element-name           Element-type
+#   Class-id        Element-name           Element-type     Element-level
 class_ids = {
 
     # EBML Basics
-    0x1a45dfa3:     ('EBML',               types.sub_elements),
-    0x4282:         ('DocType',            types.string),
-    0x4287:         ('DocTypeVersion',     types.u_integer),
-    0x4285:         ('DocTypeReadVersion', types.u_integer),
+    0x1a45dfa3:     ('EBML',               types.sub_elements,  0),
+    0x4282:         ('DocType',            types.string,    1),
+    0x4287:         ('DocTypeVersion',     types.u_integer, 1),
+    0x4285:         ('DocTypeReadVersion', types.u_integer, 1),
         
     # Global data
-    0xEC:           ('Void',  types.binary),
+    0xEC:           ('Void',  types.binary, 1),
     
     # The main elements (level 0)
-    0x18538067:     ('Segment',     types.sub_elements), # Segment
-    0x114D9B74:     ('SeekHead',    types.sub_elements), # Meta Seek Information
-    0x1549a966:     ('Info',        types.sub_elements), # Segment Information
-    0x1F43B675:     ('Cluster',     types.sub_elements), # Cluster
-    0x1c53bb6b:     ('Cues',        types.sub_elements),    
+    0x18538067:     ('Segment',     types.sub_elements, 0), # Segment
+    0x114D9B74:     ('SeekHead',    types.sub_elements, 1), # Meta Seek Information
+    0x1549a966:     ('Info',        types.sub_elements, 1), # Segment Information
+    0x1F43B675:     ('Cluster',     types.sub_elements, 1), # Cluster
+    0x1c53bb6b:     ('Cues',        types.sub_elements, 1),    
         
     # Tracks (This is were we are interested in)
-    0x1654AE6B:     ('Tracks',          types.sub_elements), 
-    0xAE:           ('TrackEntry',      types.sub_elements),
-    0xD7:           ('TrackNumber',     types.u_integer),
-    0x73C5:         ('TrackUID',        types.u_integer),
-    0x83:           ('TrackType',       types.u_integer),
-    0xB9:           ('FlagEnabled',     types.u_integer),
-    0x88:           ('FlagDefault',     types.u_integer),
-    0x55AA:         ('FlagForced',      types.u_integer),
-    0x9C:           ('FlagLacing',      types.u_integer),
-    0x6DE7:         ('MinCache',        types.u_integer),
-    0x6DF8:         ('MaxCache',        types.u_integer),
-    0x23E383:       ('DefaultDuration', types.u_integer),
-    0x23314F:       ('TrackTimecodeScale', types.float),
+    0x1654AE6B:     ('Tracks',          types.sub_elements, 1), 
+    0xAE:           ('TrackEntry',      types.sub_elements, 2),
+    0xD7:           ('TrackNumber',     types.u_integer,    3),
+    0x73C5:         ('TrackUID',        types.u_integer,    3),
+    0x83:           ('TrackType',       types.u_integer,    3),
+    0xB9:           ('FlagEnabled',     types.u_integer,    3),
+    0x88:           ('FlagDefault',     types.u_integer,    3),
+    0x55AA:         ('FlagForced',      types.u_integer,    3),
+    0x9C:           ('FlagLacing',      types.u_integer,    3),
+    0x6DE7:         ('MinCache',        types.u_integer,    3),
+    0x6DF8:         ('MaxCache',        types.u_integer,    3),
+    0x23E383:       ('DefaultDuration', types.u_integer,    3),
+    0x23314F:       ('TrackTimecodeScale', types.float,     3),
     
-    0x55EE:         ('MaxBlockAdditionID',  types.u_integer),
-    0x536E:         ('Name',                types.utf_8),
-    0x22B59C:       ('Language',            types.string),
-    0x86:           ('CodecID',             types.string),
-    0x63A2:         ('CodecPrivate',        types.binary),
-    0x258688:       ('CodecName',           types.utf_8),
-    0x7446:         ('AttachmentLink',      types.u_integer),
-    0xAA:           ('CodecDecodeAll',      types.u_integer),
+    0x55EE:         ('MaxBlockAdditionID',  types.u_integer, 3),
+    0x536E:         ('Name',                types.utf_8,     3),
+    0x22B59C:       ('Language',            types.string,    3),
+    0x86:           ('CodecID',             types.string,    3),
+    0x63A2:         ('CodecPrivate',        types.binary,    3),
+    0x258688:       ('CodecName',           types.utf_8,     3),
+    0x7446:         ('AttachmentLink',      types.u_integer, 3),
+    0xAA:           ('CodecDecodeAll',      types.u_integer, 3),
     
     # Video specific elements
-    0xE0:           ('Video',           types.sub_elements),
-    0x1a:           ('FlagInterlaced',  types.u_integer),
-    0xB0:           ('PixelWidth',      types.u_integer),
-    0xBA:           ('PixelHeight',     types.u_integer),
-    0x54B0:         ('DisplayWidth',    types.u_integer),
-    0x54BA:         ('DisplayHeight',   types.u_integer),
-    0x9A:           ('Unknown',         types.u_integer),
+    0xE0:           ('Video',           types.sub_elements, 3),
+    0x1a:           ('FlagInterlaced',  types.u_integer,    4),
+    0xB0:           ('PixelWidth',      types.u_integer,    4),
+    0xBA:           ('PixelHeight',     types.u_integer,    4),
+    0x54B0:         ('DisplayWidth',    types.u_integer,    4),
+    0x54BA:         ('DisplayHeight',   types.u_integer,    4),
+    0x9A:           ('Unknown',         types.u_integer,    4),
 
     # Audio specific elements
-    0xE1:           ('Audio', types.sub_elements),           
-    0xB5:           ('SamplingFrequency', types.float),
-    0x9F:           ('Channels', types.u_integer),
+    0xE1:           ('Audio', types.sub_elements,       3),        
+    0xB5:           ('SamplingFrequency', types.float,  4),
+    0x9F:           ('Channels', types.u_integer,       4),
+    0x6264:         ('BitDepth', types.u_integer,       4),
 }
 
 
@@ -122,7 +128,7 @@ class Parser(plugins.BaseParser):
 
         
     def parse(self, filename, video):
-        in_tracks_element = False
+        
         
         stream = streams.factory.create_filestream(filename,
                                                    endianess=self._endianess)
@@ -130,58 +136,87 @@ class Parser(plugins.BaseParser):
         # Check if this is an EBML file
         if stream.read_uint32() != 0x1a45dfa3:
             return False
+        stream.seek(0)
         
         video.set_container('matroska')
         
-        stream.seek(0)
-        video_stream = None
+        tree = self._build_tree(stream)
+        
+        self._extract_information(tree, video)
+
+
+
+    def _build_tree(self, stream):
+        """ Iterate over all the elements in the file and create a tree out of
+            it. """
+
+                
+        parsed_tracks_element = False
+
+        previous_element = self.LevelElement()
+        previous_element.key = 'Root'
+        previous_element.parent = None
+        previous_element.level = -1
+        root_elm = previous_element
+        
         for elm in self.parse_header(stream):
-            
             if elm is None:
-                
-                # Stop iterating when we have read the tracks information
-                # TODO, this should be made more error-proof
-                if in_tracks_element:
-                    break
-                
                 continue
+
+            # Create element
+            obj = self.LevelElement()
+            obj.key, obj.value, obj.level = elm
             
-            key, value = elm
-            if key == 'Tracks':
-                in_tracks_element = True
-                continue
+            if parsed_tracks_element and obj.level == 1:
+                break
             
-            if key == 'TrackType':
-                    
-                if value == 1:
-                    video_stream = video.new_video_stream()
-                elif value == 2:
-                    video_stream = video.new_audio_stream()
+            if obj.key == 'Tracks':
+                parsed_tracks_element = True
             
-            if not video_stream:
-                continue
-            
-            if video_stream.type == 'Video':
-                if key == 'PixelWidth':
-                    video_stream.set_width(value)
-                if key == 'PixelHeight':
-                    video_stream.set_height(value)
+            # We are going back in the tree
+            if obj.level < previous_element.level:
+                while obj.level <= previous_element.level:
+                    previous_element = previous_element.parent
+                obj.parent = previous_element
                 
-                if key == 'CodecID':
-                    video_stream.set_codec(value)
+            # We are going deeper in the tree (child of previous_element)
+            elif obj.level > previous_element.level:
+                obj.parent = previous_element
+                
+            # We are on the same level als the previous_element
+            else:
+                obj.parent = previous_element.parent
+
+            obj.parent.childs.append(obj)
+            previous_element = obj
             
-            elif video_stream.type == 'Audio':
-                if key == 'CodecID':
-                    video_stream.set_codec(value)
+        return root_elm
+
+    
+    def _extract_information(self, tree, video):
+        for track in tree.Segment[0].Tracks[0].TrackEntry:
+            if track.TrackType[0].value == 1:
+                stream = video.new_video_stream()
                 
-                if key == 'Channels':
-                    video_stream.set_channels(value)
+                vid = track.Video[0]
+                stream.set_width(vid.PixelWidth[0].value)
+                stream.set_height(vid.PixelHeight[0].value)
                 
-                if key == 'SamplingFrequency':
-                    video_stream.set_sample_rate(int(value))
+                timecodescale = track.TrackTimecodeScale[0].value
+                stream.set_framerate(23.976 * timecodescale)
+                stream.set_duration(seconds=track.DefaultDuration[0].value /
+                                    (1000000.0 * timecodescale))
+                
+                stream.set_codec(track.CodecID[0].value)
+            
+            if track.TrackType[0].value == 2:
+                stream = video.new_audio_stream()
+                audio = track.Audio[0]
+                stream.set_channels(audio.Channels[0].value)
+                stream.set_sample_rate(audio.SamplingFrequency[0].value)
+                stream.set_codec(track.CodecID[0].value)
         
-        
-        return True
+
             
     
     
@@ -189,11 +224,9 @@ class Parser(plugins.BaseParser):
         
         # Elements incorporate an Element ID, a descriptor for the size of the element, and the binary data itself.
         
-        while True:
+        while stream.bytes_left():
             # Fetch the element id
             octet = stream.read_byte()
-            if octet == '':
-                raise StopIteration()
             
             classid_size, classid_bytes = self.parse_octet(octet)
             
@@ -203,7 +236,6 @@ class Parser(plugins.BaseParser):
             else:
                 class_id = ord(octet)
             
-            
             # Fetch the descriptor for the size of the element
             octet = stream.read_byte()
             length_bytes, length =  self.parse_octet(octet)
@@ -212,7 +244,7 @@ class Parser(plugins.BaseParser):
             
             try:
                 value = None
-                class_name, class_type = class_ids[class_id]
+                class_name, class_type, class_level = class_ids[class_id]
                 
                 if class_type == types.string:
                     value = stream.read(length)
@@ -232,14 +264,14 @@ class Parser(plugins.BaseParser):
                 elif class_type == types.sub_elements:
                     if class_name in ['Info', 'SeekHead', 'Cluster', 'Cues']:
                         stream.seek(stream.tell() + length)
-                        yield None
+                        continue
                     
-                yield (class_name, value)
+                yield (class_name, value, class_level)
                 
             except KeyError:
                 #raise AssertionError("Unhandled class-id: %s" % hex(class_id))
                 print "Unhandled class-id: %s" % hex(class_id)
-                yield None
+                continue
     
     
     def parse_octet(self, octet):
@@ -266,6 +298,39 @@ class Parser(plugins.BaseParser):
 
         # return the bytesize and the value with the marker bit xor'ed out
         return (length, octet ^ 2**(8-length))
+
+
+    class LevelElement(object):
+        __slots__ = ['key', 'value', 'level', 'childs', 'parent'    ]
+        
+        def __init__(self):
+            self.childs = []
+
+        def __repr__(self):
+            
+            buffer = "  " * (self.level + 1) + self.key + "\n"
+            for child in self.childs:
+                buffer += repr(child)
+            return buffer
+        
+        def __getattr__(self, key):
+            if key == 'value':
+                return self.value
+            
+            if key == 'key':
+                return self.key
+            
+            
+            items = []
+            
+            for child in self.childs:
+                if child.key == key:
+                    items.append(child)
+            
+            if len(items) == 0:
+                raise AttributeError(key)
+            return items
+
     
 if __name__ == "__main__":
     import sys
